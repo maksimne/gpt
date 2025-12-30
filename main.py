fd91b875c0532198258de2d62f79e2a515abf92d
godon <godonukm@gmail.com>
2025-12-30 18:27:36 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from google import genai
+from google.genai.errors import APIError
+
+
+def res(prompt: str) -> str:
+
+    try:
+        client = genai.Client(api_key="AIzaSyC1oted8kY76A63xR8Mezg3-040-9sJXTI")
+        model_name = 'gemini-2.5-flash'
+        response = client.models.generate_content(
+            model=model_name,
+            contents=prompt
+        )
+        return response.text
+
+    except APIError as e:
+        return f"ОШИБКА API: Произошла ошибка при вызове Gemini API: {e}"
+    except Exception as e:
+        return f"Неизвестная ошибка: {e}"   
+    
+print(get_wellness_advice("hello"))
